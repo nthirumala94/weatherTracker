48685e1983b2a515b6d2bc3e8e6908f95d6d0d7c
HackerRank <hackerrank@example.com>
2018-06-02 15:01:43 +0000

    Saved at Sat Jun 02 2018 11:01:43 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/model/Measurements.java
+++ src/main/java/com/capitalone/weathertracker/model/Measurements.java
@@ -1,6 +1,6 @@
-import java.time.DateTime;
+import java.time.LocalDateTime;
 public class Measurements {
-    public DateTime timestamp;
+    public LocalDateTime timestamp;
     public float temperature;
     public float dewPoint;
     public float precipation;
