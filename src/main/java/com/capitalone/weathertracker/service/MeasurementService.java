fd36d5b460bee6f1fb4866c5262b2858c929a3d1
HackerRank <hackerrank@example.com>
2018-06-02 15:28:25 +0000

    Saved at Sat Jun 02 2018 11:28:25 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/service/MeasurementService.java
+++ src/main/java/com/capitalone/weathertracker/service/MeasurementService.java
@@ -1,4 +1,5 @@
 import java.time.LocalDateTime;
+import com.capitalone.weathertracker.model.Metrics;
 public interface MeasurementService {
     void addMeasurement(LocalDateTime timestamp, Metrics metrics);
 }
