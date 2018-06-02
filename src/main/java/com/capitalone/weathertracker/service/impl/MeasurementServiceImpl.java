777a2a532e07c12282c4ddc65d734f91456c92a0
HackerRank <hackerrank@example.com>
2018-06-02 19:14:06 +0000

    Saved at Sat Jun 02 2018 15:14:06 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/service/impl/MeasurementServiceImpl.java
+++ src/main/java/com/capitalone/weathertracker/service/impl/MeasurementServiceImpl.java
@@ -2,7 +2,7 @@ package com.capitalone.weathertracker.service.impl;
 
 import com.capitalone.weathertracker.service.MeasurementService;
 import java.time.LocalDateTime;
-import com.capitalone.weathertracker.model.Metrics;
+import com.capitalone.weathertracker.model.*;
 import java.util.*;
 
 public class MeasurementServiceImpl implements MeasurementService {
@@ -13,5 +13,42 @@ public class MeasurementServiceImpl implements MeasurementService {
         for(Map.Entry<LocalDateTime, Metrics> entry : weatherData.entrySet()) {
             System.out.println("Key: " + entry.getKey() + " value: " + entry.getValue().convertToString());
         }
+    }
+    
+    @Override
+    private ArrayList<Measurements> getMeasurement(LocalDateTime timestamp) {
+        List<Measurements> result = new ArrayList<>();
+        if(timestamp.toString().length() > 10) {
+			Metrics metricData = weatherData.get(timestamp);
+			
+			if(metricData != null) {
+			    Measurements m = new Measurements(
+			        timestamp,
+			        metricData.getTemperature(),
+			        metricData.getDewPoint(),
+			        metricData.getPrecipation()
+			    );
+			    result.add(m);
+			}
+		} else {
+		    Iterator<Map.Entry<LocalDateTime, Metrics>> iterator = weatherData.entrySet().iterator();
+		    while(iterator.hasNext()) {
+		        Map.Entry<LocalDateTime, Metrics> entry = iterator.next();
+		        if(entry.getKey().getYear() == timestamp.getYear() &&
+		        entry.getKey().getMonth() == timestamp.getMonth() &&
+		        entry.getKey().getDayOfMonth() == timestamp.getDayOfMonth()
+		        ) {
+		            
+		            Measurements m = new Measurements(
+			        timestamp,
+			        entry.getValue().getTemperature(),
+			        entry.getValue().getDewPoint(),
+			        entry.getValue().getPrecipation()
+			    );
+		            result.add(m);
+		        }
+		    }
+		}
+        return result;
     }
 }
