d65802b555211cdec8adb35fe06057fc7d1d7df4
HackerRank <hackerrank@example.com>
2018-06-02 15:19:13 +0000

    Saved at Sat Jun 02 2018 11:19:12 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/model/Measurements.java
+++ src/main/java/com/capitalone/weathertracker/model/Measurements.java
@@ -1,7 +1,36 @@
 import java.time.LocalDateTime;
-public class Measurements {
-    public LocalDateTime timestamp;
-    public float temperature;
-    public float dewPoint;
-    public float precipation;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement
+public class Measurements implements Serializable {
+    private LocalDateTime timestamp;
+    private float temperature;
+    private float dewPoint;
+    private float precipation;
+    
+    public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+	public float getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(float temperature) {
+		this.temperature = temperature;
+	}
+	public float getDewPoint() {
+		return dewPoint;
+	}
+	public void setDewPoint(float dewPoint) {
+		this.dewPoint = dewPoint;
+	}
+	public float getPrecipation() {
+		return precipation;
+	}
+	public void setPrecipation(float precipation) {
+		this.precipation = precipation;
+	}
 }
