da465d1253474df44c5a071067b09b2960ffbbf9
HackerRank <hackerrank@example.com>
2018-06-02 14:53:37 +0000

    Saved at Sat Jun 02 2018 10:53:37 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/model/Measurements.java
+++ src/main/java/com/capitalone/weathertracker/model/Measurements.java
@@ -1,3 +1,7 @@
+import org.joda.time.DateTime;
 public class Measurements {
-    
+    public DateTime timestamp;
+    public float temperature;
+    public float dewPoint;
+    public float precipation;
 }
