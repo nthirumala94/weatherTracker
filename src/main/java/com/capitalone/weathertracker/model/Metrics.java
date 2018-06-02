8d342edbc4bcb292be670a4ca0067ab8c5e143ed
HackerRank <hackerrank@example.com>
2018-06-02 15:27:24 +0000

    Saved at Sat Jun 02 2018 11:27:23 GMT-0400 (Eastern Daylight Time)

---
+++ src/main/java/com/capitalone/weathertracker/model/Metrics.java
@@ -0,0 +1,24 @@
+public class Metrics {
+    public float temperature;
+    public float dewPoint;
+    public float precipation;
+
+	public float getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(float temperature) {
+		this.temperature = temperature;
+	}
+	public float getDewPoint() {
+		return dewPoint;
+	}
+	public void setDewPoint(float dewPoint) {
+		this.dewPoint = dewPoint;
+	}
+	public float getPrecipation() {
+		return precipation;
+	}
+	public void setPrecipation(float precipation) {
+		this.precipation = precipation;
+	}
+}
