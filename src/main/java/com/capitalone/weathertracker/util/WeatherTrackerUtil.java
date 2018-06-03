e76541fd464f1188d5f3617991d170aa19a5ec4b
HackerRank <hackerrank@example.com>
2018-06-03 15:28:11 +0000

    Saved at Sun Jun 03 2018 11:28:10 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/util/WeatherTrackerUtil.java
+++ src/main/java/com/capitalone/weathertracker/util/WeatherTrackerUtil.java
@@ -1,18 +1,18 @@
 package com.capitalone.weathertracker.util;
 
-import java.time.*;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class WeatherTrackerUtil {
     
-    public static ZonedDateTime convertStringToLocalDate(String dateTimeStr) {
+    public static LocalDateTime convertStringToLocalDate(String dateTimeStr) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
-		ZonedDateTime dateTime = ZonedDateTime.parse(dateTimeStr, formatter);
+		LocalDateTime dateTime = LocalDateTime.parse(dateTimeStr, formatter);
 		return dateTime;
     }
     
     //2015-09-01T16:30:00.000Z
-    public static String convertLocalDateToString(ZonedDateTime timestamp) {
+    public static String convertLocalDateToString(LocalDateTime timestamp) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
         String formatDateTime = timestamp.format(formatter) + "Z";
         return formatDateTime;
