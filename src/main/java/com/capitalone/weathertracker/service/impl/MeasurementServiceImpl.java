2a78e7f0830a8c17d011eca4552d60fd8058b3a6
HackerRank <hackerrank@example.com>
2018-06-03 15:28:37 +0000

    Saved at Sun Jun 03 2018 11:28:37 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/service/impl/MeasurementServiceImpl.java
+++ src/main/java/com/capitalone/weathertracker/service/impl/MeasurementServiceImpl.java
@@ -12,14 +12,14 @@ public class MeasurementServiceImpl implements MeasurementService {
     
     private MeasurementServiceImpl() {}
     
-    private Map<String, Metrics> weatherData = new LinkedHashMap<>();
+    private Map<LocalDateTime, Metrics> weatherData = new LinkedHashMap<>();
     
     public static MeasurementServiceImpl getInstance() {
       return measurementServiceImpl;
    }
     
     @Override
-    public void addMeasurement(String timestamp, Metrics metrics) {
+    public void addMeasurement(LocalDateTime timestamp, Metrics metrics) {
         weatherData.put(timestamp, metrics);
         System.out.println("Size of weatherDate in addMeasurement" + weatherData.size());
     }
@@ -30,7 +30,7 @@ public class MeasurementServiceImpl implements MeasurementService {
         System.out.println("Entering getMeasurement size of weatherDate: " + weatherData.size());
         ArrayList<Measurements> result = new ArrayList<>();
         if(timestamp.toString().length() > 10) {
-            ZonedDateTime dateTimestamp = WeatherTrackerUtil.convertStringToLocalDate(timestamp);
+            LocalDateTime dateTimestamp = WeatherTrackerUtil.convertStringToLocalDate(timestamp);
             
 			Metrics metricData = weatherData.get(dateTimestamp);
 			
@@ -45,23 +45,18 @@ public class MeasurementServiceImpl implements MeasurementService {
 			}
 		} else {
 		    System.out.println("Entering Else, weatherData size" + weatherData.size());
-		    Iterator<Map.Entry<String, Metrics>> iterator = weatherData.entrySet().iterator();
+		    Iterator<Map.Entry<LocalDateTime, Metrics>> iterator = weatherData.entrySet().iterator();
 		    LocalDate localTimestamp = LocalDate.parse(timestamp);
 		    while(iterator.hasNext()) {
-		        Map.Entry<String, Metrics> entry = iterator.next();
+		        Map.Entry<LocalDateTime, Metrics> entry = iterator.next();
 		        System.out.println("Entry Key in loop: " + entry.getKey());
-		        String[] entryKeySplit = entry.getKey().split("(?:-|T)");
-		        int year = Integer.parseInt(entryKeySplit[0]);
-		        int month = Integer.parseInt(entryKeySplit[1]);
-		        int day = Integer.parseInt(entryKeySplit[2]);
-		        System.out.println("Year : " + year + " month " + month + " day " + day);
-		        if(year == localTimestamp.getYear() &&
-		        month == localTimestamp.getMonth().getValue() &&
-		        day == localTimestamp.getDayOfMonth()
+		        if(entry.getKey().getYear() == localTimestamp.getYear() &&
+		        entry.getKey().getMonth() == localTimestamp.getMonth() &&
+		        entry.getKey().getDayOfMonth() == localTimestamp.getDayOfMonth()
 		        ) {
 		            System.out.println("Match found");
 		            Measurements m = new Measurements(
-			        entry.getKey(),
+			        WeatherTrackerUtil.convertLocalDateToString(entry.getKey()),
 			        entry.getValue().getTemperature(),
 			        entry.getValue().getDewPoint(),
 			        entry.getValue().getPrecipation()
@@ -72,37 +67,5 @@ public class MeasurementServiceImpl implements MeasurementService {
 		}
 		System.out.println(result.size());
         return result;
-    }
-    
-    @Override
-    public Measurements updateMeasurement(String timestamp, Metrics metrics, boolean isValidRequest, boolean entryExists) {
-    	Measurements result = null;
-    	if(weatherData.keySet().contains(timestamp)) {
-    		entryExists = false;
-    	}
-    	if(isValidRequest) {
-    		weatherData.put(timestamp, metrics);
-    		result = new Measurements(
-    				timestamp.toString(),
-    				metrics.getTemperature(),
-    				metrics.getDewPoint(),
-    				metrics.getPrecipation()
-    				);
-    	} else {
-    		Metrics existingMetric = weatherData.get(timestamp);
-    		System.out.println("The error with Metric: " + existingMetric);
-    		result = new Measurements(
-    				timestamp.toString(),
-    				existingMetric.getTemperature(),
-    				existingMetric.getDewPoint(),
-    				existingMetric.getPrecipation()
-    				);
-    	}
-    	return result;
-    }
-    
-    @Override
-    public void deleteMeasurement(String timestamp) {
-    	weatherData.remove(timestamp);
     }
 }
