d5b2a0b832f1c8901089379564b4ab59d41f67b7
HackerRank <hackerrank@example.com>
2018-06-03 16:03:05 +0000

    Saved at Sun Jun 03 2018 12:03:05 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/util/WeatherTrackerUtil.java
+++ src/main/java/com/capitalone/weathertracker/util/WeatherTrackerUtil.java
@@ -1,20 +1,34 @@
 package com.capitalone.weathertracker.util;
 
-import java.time.LocalDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import com.capitalone.weathertracker.model.Measurements;
 
 public class WeatherTrackerUtil {
     
-    public static LocalDateTime convertStringToLocalDate(String dateTimeStr) {
+    public static ZonedDateTime convertStringToLocalDate(String dateTimeStr) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
-		LocalDateTime dateTime = LocalDateTime.parse(dateTimeStr, formatter);
+		ZonedDateTime dateTime = ZonedDateTime.parse(dateTimeStr, formatter);
 		return dateTime;
     }
     
     //2015-09-01T16:30:00.000Z
-    public static String convertLocalDateToString(LocalDateTime timestamp) {
+    public static String convertLocalDateToString(ZonedDateTime timestamp) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
         String formatDateTime = timestamp.format(formatter) + "Z";
         return formatDateTime;
+    }
+    
+    public void sortMeasurementList(ArrayList<Measurements> measurementList) {
+    	Collections.sort(measurementList, new Comparator<Measurements>() {
+    		@Override
+    	    public int compare(Measurements m1, Measurements m2) {
+    	        return m1.getTimestamp().compareTo(m2.getTimestamp());
+    	    }
+    	});
     }
 }
