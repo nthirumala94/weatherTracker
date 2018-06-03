5fe84a698dac1b3e44d372b9cf8fae5786dda2ab
HackerRank <hackerrank@example.com>
2018-06-03 07:53:34 +0000

    Saved at Sun Jun 03 2018 03:53:34 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/service/MeasurementService.java
+++ src/main/java/com/capitalone/weathertracker/service/MeasurementService.java
@@ -1,11 +1,11 @@
 package com.capitalone.weathertracker.service;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import com.capitalone.weathertracker.model.*;
 import java.util.*;
 
 public interface MeasurementService {
-     void addMeasurement(LocalDateTime timestamp, Metrics metrics);
+     void addMeasurement(ZonedDateTime timestamp, Metrics metrics);
      ArrayList<Measurements> getMeasurement(String timestamp);
-     Measurements updateMeasurement(LocalDateTime timestamp, Metrics metrics, boolean isValidRequest, boolean entryExists);
+     Measurements updateMeasurement(ZonedDateTime timestamp, Metrics metrics, boolean isValidRequest, boolean entryExists);
 }
