8676ca3e506f8cd717021a924b55aa7651acddad
HackerRank <hackerrank@example.com>
2018-06-04 17:47:10 +0000

    Saved at Mon Jun 04 2018 13:47:09 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/util/WeatherTrackerUtil.java
+++ src/main/java/com/capitalone/weathertracker/util/WeatherTrackerUtil.java
@@ -1,6 +1,7 @@
 package com.capitalone.weathertracker.util;
 
 import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
