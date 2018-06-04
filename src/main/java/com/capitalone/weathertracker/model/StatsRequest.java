d67e6af27f07bea2b6f213913f6b03bb7eda3b9e
HackerRank <hackerrank@example.com>
2018-06-04 02:12:49 +0000

    Saved at Sun Jun 03 2018 22:12:48 GMT-0400 (Eastern Daylight Time)

---
+++ src/main/java/com/capitalone/weathertracker/model/StatsRequest.java
@@ -0,0 +1,46 @@
+package com.capitalone.weathertracker.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class StatsRequest {
+
+    private ArrayList<String> metric;
+    private ArrayList<String> stats;
+    private LocalDate fromDateTime;
+    private LocalDate toDateTime;
+
+    public ArrayList<String> getMetric() {
+        return metric;
+    }
+
+    public void setMetric(ArrayList<String> metric) {
+        this.metric = metric;
+    }
+
+    public ArrayList<String> getStats() {
+        return stats;
+    }
+
+    public void setStats(ArrayList<String> stats) {
+        this.stats = stats;
+    }
+
+    public LocalDate getFromDateTime() {
+        return fromDateTime;
+    }
+
+    public void setFromDateTime(LocalDate fromDateTime) {
+        this.fromDateTime = fromDateTime;
+    }
+
+    public LocalDate getToDateTime() {
+        return toDateTime;
+    }
+
+    public void setToDateTime(LocalDate toDateTime) {
+        this.toDateTime = toDateTime;
+    }
+
+
+}
