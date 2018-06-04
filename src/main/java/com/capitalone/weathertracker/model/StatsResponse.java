1944a06c83c99e7b3cad7aa34ed0c825a6253257
HackerRank <hackerrank@example.com>
2018-06-04 02:13:37 +0000

    Saved at Sun Jun 03 2018 22:13:36 GMT-0400 (Eastern Daylight Time)

---
+++ src/main/java/com/capitalone/weathertracker/model/StatsResponse.java
@@ -0,0 +1,42 @@
+package com.capitalone.weathertracker.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class StatsResponse {
+
+    private String metric;
+    private String stat;
+    private float value;
+
+    public StatsResponse(String metric, String stat, float value){
+        this.metric = metric;
+        this.stat= stat;
+        this.value = value;
+    }
+
+    public StatsResponse(){
+    }
+
+    public String getMetric() {
+        return metric;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setMetric(String metric) {
+        this.metric = metric;
+    }
+
+    public void setStat(String stat) {
+        this.stat = stat;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+
+}
