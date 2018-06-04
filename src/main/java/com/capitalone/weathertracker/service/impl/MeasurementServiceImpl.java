e3abd42e6f7cd79f447a101d1cf275144ca4c9f0
HackerRank <hackerrank@example.com>
2018-06-04 04:08:59 +0000

    Saved at Mon Jun 04 2018 00:08:59 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/service/impl/MeasurementServiceImpl.java
+++ src/main/java/com/capitalone/weathertracker/service/impl/MeasurementServiceImpl.java
@@ -162,24 +162,22 @@ public class MeasurementServiceImpl implements MeasurementService {
 								}
 								break;
 							case "dewPoint":
-								System.out.println("StatsEntry1: " + statsEntry.getValue().getDewPoint());
-								System.out.println("StatusResponse1: " + statsResp.getValue());
+
 								if(statsResp.getValue() == 0.0f) {
 									statsResp.setValue(statsEntry.getValue().getDewPoint());
 								}
-								System.out.println("StatsEntry2: " + statsEntry.getValue().getDewPoint());
-								System.out.println("StatusResponse2: " + statsResp.getValue());
+
 								if (statsEntry.getValue().getDewPoint() != 0.0f && statsResp.getValue() > statsEntry.getValue().getDewPoint()) {
 									statsResp.setValue(statsEntry.getValue().getDewPoint());
 								}
-								System.out.println("StatsEntry3: " + statsEntry.getValue().getDewPoint());
-								System.out.println("StatusResponse3: " + statsResp.getValue());
+
 								break;
 							case "precipitation":
 								if(statsResp.getValue() == 0.0f) {
 									statsResp.setValue(statsEntry.getValue().getPrecipation());
 								}
-								if (statsResp.getValue() > statsEntry.getValue().getPrecipation()) {
+
+								if (statsEntry.getValue().getPrecipation() != 0.0f && statsResp.getValue() > statsEntry.getValue().getPrecipation()) {
 									statsResp.setValue(statsEntry.getValue().getPrecipation());
 								}
 								break;
@@ -193,17 +191,27 @@ public class MeasurementServiceImpl implements MeasurementService {
 						Map.Entry<String, Metrics> statsEntry = statsIterator.next();
 						switch (metric) {
 							case "temperature":
-								if (statsResp.getValue() < statsEntry.getValue().getTemperature()) {
+								if(statsResp.getValue() == 0.0f) {
+									statsResp.setValue(statsEntry.getValue().getTemperature());
+								}
+								if (statsEntry.getValue().getTemperature() != 0.0f && statsResp.getValue() < statsEntry.getValue().getTemperature()) {
 									statsResp.setValue(statsEntry.getValue().getTemperature());
 								}
 								break;
 							case "dewPoint":
-								if (statsResp.getValue() < statsEntry.getValue().getDewPoint()) {
+								if(statsResp.getValue() == 0.0f) {
+									statsResp.setValue(statsEntry.getValue().getDewPoint());
+								}
+
+								if (statsEntry.getValue().getDewPoint() != 0.0f && statsResp.getValue() < statsEntry.getValue().getDewPoint()) {
 									statsResp.setValue(statsEntry.getValue().getDewPoint());
 								}
 								break;
 							case "precipitation":
-								if (statsResp.getValue() < statsEntry.getValue().getPrecipation()) {
+								if(statsResp.getValue() == 0.0f) {
+									statsResp.setValue(statsEntry.getValue().getPrecipation());
+								}
+								if (statsEntry.getValue().getPrecipation() != 0.0f && statsResp.getValue() < statsEntry.getValue().getPrecipation()) {
 									statsResp.setValue(statsEntry.getValue().getPrecipation());
 								}
 								break;
