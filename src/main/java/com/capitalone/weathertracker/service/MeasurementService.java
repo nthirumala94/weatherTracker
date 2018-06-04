acb4fda310c598f9f4ddb7510092aa053c2240c6
HackerRank <hackerrank@example.com>
2018-06-04 01:04:04 +0000

    Saved at Sun Jun 03 2018 21:04:04 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/service/MeasurementService.java
+++ src/main/java/com/capitalone/weathertracker/service/MeasurementService.java
@@ -9,4 +9,5 @@ public interface MeasurementService {
      ArrayList<Measurements> getMeasurement(String timestamp);
      Metrics deleteMeasurement(String timestamp);
      int updateMeasurement(String timestamp, Metrics metrics);
+     int patchMeasurement(String timestamp, Metrics metrics);
 }
