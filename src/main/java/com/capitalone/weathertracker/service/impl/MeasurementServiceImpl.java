adb4c63c59238739f90fb389391a03ed1d2ca0e0
HackerRank <hackerrank@example.com>
2018-06-05 01:19:42 +0000

    Saved at Mon Jun 04 2018 21:19:41 GMT-0400 (Eastern Daylight Time)

--- src/main/java/com/capitalone/weathertracker/service/impl/MeasurementServiceImpl.java
+++ src/main/java/com/capitalone/weathertracker/service/impl/MeasurementServiceImpl.java
@@ -7,182 +7,182 @@ import java.util.*;
 import com.capitalone.weathertracker.util.WeatherTrackerUtil;
 
 public class MeasurementServiceImpl implements MeasurementService {
-
-	private static MeasurementServiceImpl measurementServiceImpl = new MeasurementServiceImpl();
-
-	private MeasurementServiceImpl() {}
-
-	private Map<String, Metrics> weatherData = new LinkedHashMap<>();
-
-	public static MeasurementServiceImpl getInstance() {
-		return measurementServiceImpl;
-	}
-
-	@Override
-	public void addMeasurement(String timestamp, Metrics metrics) {
-		System.out.println("Here is the timestamp: " + timestamp + ", metrics" + metrics);
-		weatherData.put(timestamp, metrics);
-		System.out.println("Size of weatherDate in addMeasurement" + weatherData.size());
-	}
-
-	@Override
-	public ArrayList<Measurements> getMeasurement(String timestamp) {
-		System.out.println("Entering getMeasurement: " + timestamp);
-		System.out.println("Entering getMeasurement size of weatherDate: " + weatherData.size());
-		ArrayList<Measurements> result = new ArrayList<>();
-		if(timestamp.toString().length() > 10) {
-			LocalDateTime dateTimestamp = WeatherTrackerUtil.convertStringToLocalDate(timestamp);
-
+    
+    private static MeasurementServiceImpl measurementServiceImpl = new MeasurementServiceImpl();
+    
+    private MeasurementServiceImpl() {}
+    
+    private Map<String, Metrics> weatherData = new LinkedHashMap<>();
+    
+    public static MeasurementServiceImpl getInstance() {
+      return measurementServiceImpl;
+   }
+    
+    @Override
+    public void addMeasurement(String timestamp, Metrics metrics) {
+        System.out.println("Here is the timestamp: " + timestamp + ", metrics" + metrics);
+        weatherData.put(timestamp, metrics);
+        System.out.println("Size of weatherDate in addMeasurement" + weatherData.size());
+    }
+    
+    @Override
+    public ArrayList<Measurements> getMeasurement(String timestamp) {
+        System.out.println("Entering getMeasurement: " + timestamp);
+        System.out.println("Entering getMeasurement size of weatherDate: " + weatherData.size());
+        ArrayList<Measurements> result = new ArrayList<>();
+        if(timestamp.toString().length() > 10) {
+            LocalDateTime dateTimestamp = WeatherTrackerUtil.convertStringToLocalDate(timestamp);
+            
 			Metrics metricData = weatherData.get(timestamp);
-
+			
 			if(metricData != null) {
-				Measurements m = new Measurements(
-						timestamp,
-						metricData.getTemperature(),
-						metricData.getDewPoint(),
-						metricData.getPrecipation()
-						);
-				result.add(m);
+			    Measurements m = new Measurements(
+			        timestamp,
+			        metricData.getTemperature(),
+			        metricData.getDewPoint(),
+			        metricData.getPrecipation()
+			    );
+			    result.add(m);
 			}
 		} else {
-			System.out.println("Entering Else, weatherData size" + weatherData.size());
-			Iterator<Map.Entry<String, Metrics>> iterator = weatherData.entrySet().iterator();
-			LocalDate localTimestamp = LocalDate.parse(timestamp);
-			while(iterator.hasNext()) {
-				Map.Entry<String, Metrics> entry = iterator.next();
-				System.out.println("Entry Key in loop: " + entry.getKey());
-				String[] entryKeySplit = entry.getKey().split("(?:-|T)");
-
-				int year = Integer.parseInt(entryKeySplit[0]);
-				int month = Integer.parseInt(entryKeySplit[1]);
-				int day = Integer.parseInt(entryKeySplit[2]);
-				System.out.println("Year : " + year + " month " + month + " day " + day);
-
-				if(year == localTimestamp.getYear() &&
-						month == localTimestamp.getMonth().getValue() &&
-						day == localTimestamp.getDayOfMonth()
-						) {
-					System.out.println("Match found");
-					Measurements m = new Measurements(
-							entry.getKey(),
-							entry.getValue().getTemperature(),
-							entry.getValue().getDewPoint(),
-							entry.getValue().getPrecipation()
-							);
-					result.add(m);
-				}
-			}
+		    System.out.println("Entering Else, weatherData size" + weatherData.size());
+		    Iterator<Map.Entry<String, Metrics>> iterator = weatherData.entrySet().iterator();
+		    LocalDate localTimestamp = LocalDate.parse(timestamp);
+		    while(iterator.hasNext()) {
+		        Map.Entry<String, Metrics> entry = iterator.next();
+		        System.out.println("Entry Key in loop: " + entry.getKey());
+		        String[] entryKeySplit = entry.getKey().split("(?:-|T)");
+		        
+		        int year = Integer.parseInt(entryKeySplit[0]);
+		        int month = Integer.parseInt(entryKeySplit[1]);
+		        int day = Integer.parseInt(entryKeySplit[2]);
+		        System.out.println("Year : " + year + " month " + month + " day " + day);
+		        
+		        if(year == localTimestamp.getYear() &&
+		        month == localTimestamp.getMonth().getValue() &&
+		        day == localTimestamp.getDayOfMonth()
+		        ) {
+		            System.out.println("Match found");
+		            Measurements m = new Measurements(
+			        entry.getKey(),
+			        entry.getValue().getTemperature(),
+			        entry.getValue().getDewPoint(),
+			        entry.getValue().getPrecipation()
+			    );
+		            result.add(m);
+		        }
+		    }
 		}
 		System.out.println("Total results size: " + result.size());
 		WeatherTrackerUtil.sortMeasurementList(result);
 		for (Measurements object: result) {
-			System.out.println(object);
-		}
-		return result;
-	}
-
-	@Override
-	public Metrics deleteMeasurement(String timestamp) {
-		return weatherData.remove(timestamp);
-	}
-
-	@Override
-	public int updateMeasurement(String timestamp, Metrics metrics) {
-		if(weatherData.containsKey(timestamp)) {
-			weatherData.put(timestamp, metrics);
-			return 204;
-		} else {
-			return 404;
-		}
-	}
-
-	@Override
-	public int patchMeasurement(String timestamp, Metrics metrics) {
-		if(weatherData.containsKey(timestamp)) {
-			Metrics oldMetric = weatherData.get(timestamp);
-			if(metrics.getPrecipation() != Float.MIN_VALUE) {
-				oldMetric.setPrecipation(metrics.getPrecipation());
-			}
-
-			if(metrics.getDewPoint() != Float.MIN_VALUE) {
-				oldMetric.setDewPoint(metrics.getDewPoint());
-			}
-
-			if(metrics.getTemperature() != Float.MIN_VALUE) {
-				oldMetric.setTemperature(metrics.getTemperature());
-			}
-			weatherData.put(timestamp, oldMetric);
-			return 204;
-		} else {
-			return 404;
-		}
-	}
-
-	@Override
+            System.out.println(object);
+        }
+        return result;
+    }
+    
+    @Override
+    public Metrics deleteMeasurement(String timestamp) {
+    	return weatherData.remove(timestamp);
+    }
+    
+    @Override
+    public int updateMeasurement(String timestamp, Metrics metrics) {
+    	if(weatherData.containsKey(timestamp)) {
+    		weatherData.put(timestamp, metrics);
+        	return 204;
+    	} else {
+    		return 404;
+    	}
+    }
+    
+    @Override
+    public int patchMeasurement(String timestamp, Metrics metrics) {
+    	if(weatherData.containsKey(timestamp)) {
+    		Metrics oldMetric = weatherData.get(timestamp);
+    		if(metrics.getPrecipation() != Float.MIN_VALUE) {
+    			oldMetric.setPrecipation(metrics.getPrecipation());
+    		}
+    		
+    		if(metrics.getDewPoint() != Float.MIN_VALUE) {
+    			oldMetric.setDewPoint(metrics.getDewPoint());
+    		}
+    		
+    		if(metrics.getTemperature() != Float.MIN_VALUE) {
+    			oldMetric.setTemperature(metrics.getTemperature());
+    		}
+    		weatherData.put(timestamp, oldMetric);
+    		return 204;
+    	} else {
+    		return 404;
+    	}
+    }
+    
+    @Override
 	public ArrayList<StatsResponse> getMeasurementStatistics(StatsRequest statsRequest) {
 
 		ArrayList<StatsResponse> statsResponseList = new ArrayList<StatsResponse>();
 		Map<String, Metrics> weatherDataForStats = new LinkedHashMap<>();
-		Iterator<Map.Entry<String, Metrics>> iterator = weatherData.entrySet().iterator();
+ 		Iterator<Map.Entry<String, Metrics>> iterator = weatherData.entrySet().iterator();
 		while(iterator.hasNext()) {
 			Map.Entry<String, Metrics> entry = iterator.next();
 			LocalDateTime localTimestamp = WeatherTrackerUtil.convertStringToLocalDate(entry.getKey());
 			System.out.println("Local Time Stamp " + localTimestamp);
 			System.out.println("fromDateTime " + statsRequest.getFromDateTime());
 			System.out.println("toDateTime " + statsRequest.getToDateTime());
-
+			
 			if((localTimestamp.isEqual(statsRequest.getFromDateTime())
 					|| localTimestamp.isAfter(statsRequest.getFromDateTime()))
 					&& (localTimestamp.isBefore(statsRequest.getToDateTime())
-							)) {
+					)) {
 				weatherDataForStats.put(entry.getKey(),entry.getValue());
 			}
 		}
-
+		
 		System.out.println("Inside stats: weatherDataStats size: " + weatherDataForStats.size());
-
-		for(String metric : statsRequest.getMetric()){
+		
+    	for(String metric : statsRequest.getMetric()){
 			for(String stat : statsRequest.getStats()) {
 				StatsResponse statsResp = new StatsResponse();
 				statsResp.setMetric(metric);
 				statsResp.setStat(stat);
-
-
+				
+				
 				Iterator<Map.Entry<String, Metrics>> statsIterator = weatherDataForStats.entrySet().iterator();
 				if (stat.equalsIgnoreCase("min")) {
 					while (statsIterator.hasNext()) {
 						Map.Entry<String, Metrics> statsEntry = statsIterator.next();
 						switch (metric) {
-						case "temperature":
-							if(statsResp.getValue() == 0.0f) {
-								statsResp.setValue(statsEntry.getValue().getTemperature());
-							}
-							if (statsResp.getValue() > statsEntry.getValue().getTemperature()) {
-								statsResp.setValue(statsEntry.getValue().getTemperature());
-							}
-							break;
-						case "dewPoint":
+							case "temperature":
+								if(statsResp.getValue() == 0.0f) {
+									statsResp.setValue(statsEntry.getValue().getTemperature());
+								}
+								if (statsResp.getValue() > statsEntry.getValue().getTemperature()) {
+									statsResp.setValue(statsEntry.getValue().getTemperature());
+								}
+								break;
+							case "dewPoint":
 
-							if(statsResp.getValue() == 0.0f) {
-								statsResp.setValue(statsEntry.getValue().getDewPoint());
-							}
+								if(statsResp.getValue() == 0.0f) {
+									statsResp.setValue(statsEntry.getValue().getDewPoint());
+								}
 
-							if (statsEntry.getValue().getDewPoint() != 0.0f && statsResp.getValue() > statsEntry.getValue().getDewPoint()) {
-								statsResp.setValue(statsEntry.getValue().getDewPoint());
-							}
+								if (statsEntry.getValue().getDewPoint() != 0.0f && statsResp.getValue() > statsEntry.getValue().getDewPoint()) {
+									statsResp.setValue(statsEntry.getValue().getDewPoint());
+								}
 
-							break;
-							// 			case "precipitation":
-							// 				if(statsResp.getValue() == 0.0f) {
-							// 					statsResp.setValue(statsEntry.getValue().getPrecipation());
-							// 				}
+								break;
+				// 			case "precipitation":
+				// 				if(statsResp.getValue() == 0.0f) {
+				// 					statsResp.setValue(statsEntry.getValue().getPrecipation());
+				// 				}
 
-							// 				if (statsEntry.getValue().getPrecipation() != 0.0f && statsResp.getValue() > statsEntry.getValue().getPrecipation()) {
-							// 					statsResp.setValue(statsEntry.getValue().getPrecipation());
-							// 				}
-							// 				break;
-						default:
-							break;
+				// 				if (statsEntry.getValue().getPrecipation() != 0.0f && statsResp.getValue() > statsEntry.getValue().getPrecipation()) {
+				// 					statsResp.setValue(statsEntry.getValue().getPrecipation());
+				// 				}
+				// 				break;
+							default:
+								break;
 						}
 					}
 				}
@@ -190,33 +190,33 @@ public class MeasurementServiceImpl implements MeasurementService {
 					while (statsIterator.hasNext()) {
 						Map.Entry<String, Metrics> statsEntry = statsIterator.next();
 						switch (metric) {
-						case "temperature":
-							if(statsResp.getValue() == 0.0f) {
-								statsResp.setValue(statsEntry.getValue().getTemperature());
-							}
-							if (statsEntry.getValue().getTemperature() != 0.0f && statsResp.getValue() < statsEntry.getValue().getTemperature()) {
-								statsResp.setValue(statsEntry.getValue().getTemperature());
-							}
-							break;
-						case "dewPoint":
-							if(statsResp.getValue() == 0.0f) {
-								statsResp.setValue(statsEntry.getValue().getDewPoint());
-							}
+							case "temperature":
+								if(statsResp.getValue() == 0.0f) {
+									statsResp.setValue(statsEntry.getValue().getTemperature());
+								}
+								if (statsEntry.getValue().getTemperature() != 0.0f && statsResp.getValue() < statsEntry.getValue().getTemperature()) {
+									statsResp.setValue(statsEntry.getValue().getTemperature());
+								}
+								break;
+							case "dewPoint":
+								if(statsResp.getValue() == 0.0f) {
+									statsResp.setValue(statsEntry.getValue().getDewPoint());
+								}
 
-							if (statsEntry.getValue().getDewPoint() != 0.0f && statsResp.getValue() < statsEntry.getValue().getDewPoint()) {
-								statsResp.setValue(statsEntry.getValue().getDewPoint());
-							}
-							break;
-							// 			case "precipitation":
-							// 				if(statsResp.getValue() == 0.0f) {
-							// 					statsResp.setValue(statsEntry.getValue().getPrecipation());
-							// 				}
-							// 				if (statsEntry.getValue().getPrecipation() != 0.0f && statsResp.getValue() < statsEntry.getValue().getPrecipation()) {
-							// 					statsResp.setValue(statsEntry.getValue().getPrecipation());
-							// 				}
-							// 				break;
-						default:
-							break;
+								if (statsEntry.getValue().getDewPoint() != 0.0f && statsResp.getValue() < statsEntry.getValue().getDewPoint()) {
+									statsResp.setValue(statsEntry.getValue().getDewPoint());
+								}
+								break;
+				// 			case "precipitation":
+				// 				if(statsResp.getValue() == 0.0f) {
+				// 					statsResp.setValue(statsEntry.getValue().getPrecipation());
+				// 				}
+				// 				if (statsEntry.getValue().getPrecipation() != 0.0f && statsResp.getValue() < statsEntry.getValue().getPrecipation()) {
+				// 					statsResp.setValue(statsEntry.getValue().getPrecipation());
+				// 				}
+				// 				break;
+							default:
+								break;
 						}
 					}
 				}
@@ -231,61 +231,56 @@ public class MeasurementServiceImpl implements MeasurementService {
 					while (statsIterator.hasNext()) {
 						Map.Entry<String, Metrics> statsEntry = statsIterator.next();
 						switch (metric) {
-						case "temperature":
-							if(statsEntry.getValue().getTemperature() != 0.0f){
-								temperatureAvg=	temperatureAvg + statsEntry.getValue().getTemperature();
-								temperatureCount ++;
-							}
-							break;
-						case "dewPoint":
-							if(statsEntry.getValue().getDewPoint() != 0.0f){
-								dewpointAvg=	dewpointAvg + statsEntry.getValue().getDewPoint();
-								dewpointCount ++;
-							}
-							break;
-
-							// 			case "precipitation":
-							// 				if(statsEntry.getValue().getPrecipation() != 0.0f){
-							// 					precipAvg=	precipAvg + statsEntry.getValue().getPrecipation();
-							// 					precipCount ++;
-							// 				}
-							// 				break;
-						default:
-							break;
+							case "temperature":
+							    if(statsEntry.getValue().getTemperature() != 0.0f){
+									temperatureAvg=	temperatureAvg + statsEntry.getValue().getTemperature();
+									temperatureCount ++;
+								}
+								break;
+							case "dewPoint":
+								if(statsEntry.getValue().getDewPoint() != 0.0f){
+									dewpointAvg=	dewpointAvg + statsEntry.getValue().getDewPoint();
+									dewpointCount ++;
+								}
+								break;
+				// 			case "precipitation":
+				// 				if(statsEntry.getValue().getPrecipation() != 0.0f){
+				// 					precipAvg=	precipAvg + statsEntry.getValue().getPrecipation();
+				// 					precipCount ++;
+				// 				}
+				// 				break;
+							default:
+								break;
 						}
 					}
 					switch (metric) {
-					case "temperature":
-						if(temperatureCount!=0) temperatureAvg= temperatureAvg/temperatureCount;
-
-						temperatureAvg = (float) (Math.round(temperatureAvg * 100.0)/100.0);
-
-
-						statsResp.setValue(temperatureAvg);
-						break;
-					case "dewPoint":
-						if(dewpointCount!=0) dewpointAvg= dewpointAvg/dewpointCount;
-						dewpointAvg = (float) (Math.round(dewpointAvg * 100.0)/100.0);
-						statsResp.setValue(dewpointAvg);
-						break;
-						// 		case "precipitation":
-						// 			if(precipCount!=0) precipAvg= precipAvg/precipCount;
-						// 			statsResp.setValue(dewpointAvg);
-						// 			break;
-					default:
-						break;
+						case "temperature":
+							if(temperatureCount!=0) temperatureAvg= temperatureAvg/temperatureCount;
+							temperatureAvg = (float) (Math.round(temperatureAvg * 100.0)/100.0);
+							statsResp.setValue(temperatureAvg);
+							break;
+						case "dewPoint":
+							if(dewpointCount!=0) dewpointAvg= dewpointAvg/dewpointCount;
+							dewpointAvg = (float) (Math.round(dewpointAvg * 100.0)/100.0);
+							statsResp.setValue(dewpointAvg);
+							break;
+				// 		case "precipitation":
+				// 			if(precipCount!=0) precipAvg= precipAvg/precipCount;
+				// 			statsResp.setValue(dewpointAvg);
+				// 			break;
 					}
-					statsResponseList.add(statsResp);
 				}
-			}
-			WeatherTrackerUtil.removeItemIfNull(statsResponseList);
-
-			Iterator<StatsResponse> statsIterator = statsResponseList.iterator();
-			while(statsIterator.hasNext()) {
-				StatsResponse resp = statsIterator.next();
-				System.out.println("StatsListInfo: " + resp.getMetric() + ", " + resp.getStat() + ", " + resp.getValue());
+				statsResponseList.add(statsResp);
 			}
 		}
-		return statsResponseList;
+		WeatherTrackerUtil.removeItemIfNull(statsResponseList);
+		
+		Iterator<StatsResponse> statsIterator = statsResponseList.iterator();
+    	while(statsIterator.hasNext()) {
+    		StatsResponse resp = statsIterator.next();
+    		System.out.println("StatsListInfo: " + resp.getMetric() + ", " + resp.getStat() + ", " + resp.getValue());
+    	}
+		
+    	return statsResponseList;
 	}
 }
